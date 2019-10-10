a4d725c5b343546f20728448971922476fc38e42
Harvey <laozhaochaguan@gmail.com>
2019-10-10 10:30:00 +0800

    add scheme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import validator from 'option-validator';
 import Emitter from './emitter';
 import Events from './events';
+import Drawer from './drawer';
 
 let id = 0;
 class WFPlayer extends Emitter {
@@ -13,18 +14,52 @@ class WFPlayer extends Emitter {
     }
 
     static get default() {
-        return {};
+        return {
+            container: '#wfplayer',
+            mediaElement: '',
+            waveColor: '#fff',
+            backgroundColor: '#000',
+            cursor: true,
+            cursorColor: '#fff',
+            progress: true,
+            progressColor: '#fff',
+            grid: true,
+            gridColor: '#fff',
+            pixelRatio: window.devicePixelRatio,
+        };
     }
 
     static get scheme() {
-        return {};
+        return {
+            container: 'string|HTMLDivElement',
+            mediaElement: 'string|HTMLVideoElement|HTMLAudioElement',
+            waveColor: 'string',
+            backgroundColor: 'string',
+            cursor: 'boolean',
+            cursorColor: 'string',
+            progress: 'boolean',
+            progressColor: 'string',
+            grid: 'boolean',
+            gridColor: 'string',
+            pixelRatio: 'number',
+        };
     }
 
     constructor(options = {}) {
         super();
+        this.options = {};
         this.setOptions(options);
 
+        if (typeof this.options.container === 'string') {
+            this.options.container = document.querySelector(this.options.container);
+        }
+
+        if (this.options.mediaElement && typeof this.options.mediaElement === 'string') {
+            this.options.mediaElement = document.querySelector(this.options.mediaElement);
+        }
+
         this.events = new Events(this);
+        this.drawer = new Drawer(this);
 
         id += 1;
         this.id = id;
@@ -35,12 +70,17 @@ class WFPlayer extends Emitter {
         this.options = validator(
             {
                 ...WFPlayer.default,
+                ...this.options,
                 ...options,
             },
             WFPlayer.scheme,
         );
 
         return this;
+    }
+
+    seek(time) {
+        this.drawer.seek(time);
     }
 
     destroy() {
