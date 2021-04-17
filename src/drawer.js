73bc7b31382ae00a280ea07fc410ad1ff59a16b1
Harvey <laozhaochaguan@gmail.com>
2021-04-17 10:30:03 +0800

    Update drawer.js

--- src/drawer.js
+++ src/drawer.js
@@ -15,7 +15,7 @@ export default class Drawer {
 
             this.wf.events.proxy(this.worker, 'message', (event) => {
                 const { type, data } = event.data;
-                if (type === 'UPFATE') {
+                if (type === 'UPFATE' && !wf.isDestroy) {
                     this.wf.emit('update', data.config);
                     this.ctx.transferFromImageBitmap(data.imageBitmap);
                 }
